2a70fc73c3aa697a2485ffa0da5ff12da8592da4
F0NSII <86667716+F0NSII@users.noreply.github.com>
2025-05-22 20:34:58 +0000

    feat: solução d tarefa

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-// Escreva seu código aqui!
+let Ruim: boolean = true;
+let Travou: number = 8000;
+let Vontade: string = 'Tacar na parede';
+let Especs; 
+
+console.log('Computador da sala é ruim? ' + Ruim);
+console.log('Ele travou mais de ' + Travou + ', por isso tenho vontade de ' + Vontade);
+console.log('Suas especificações são ' + Especs);
